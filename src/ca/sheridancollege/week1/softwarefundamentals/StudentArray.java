464a488cf86d56a718eef2dc3c49890655229407
Manpreet Kaur <mkkhokhar96@gmail.com>
2019-01-22 16:39:42 -0500

    My Third Commit

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentArray.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentArray.java
@@ -16,13 +16,14 @@ public class StudentArray
     public static void main(String[] args)
     {
         System.out.print("Hello World");
-        StudentDemo [] studentList = new StudentDemo[5];
+        System.out.print("Another line");
+        StudentDemo [] studentList = new StudentDemo[4];
         StudentDemo student1 = new StudentDemo("1", "Jim Halpern");
         studentList[0] = student1;
         studentList[1] = new StudentDemo("2", "Michael Scott");
         studentList[2] = new StudentDemo("3", "Pam Beasley");
         studentList[3] = new StudentDemo("4", "Dwight Schrute");
-        studentList[4] = new StudentDemo("5", "Toby Flendersen");
+        
         
         for(int i=0; i<studentList.length; i++)
         {
